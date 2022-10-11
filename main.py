6bee5e3bd6aea433bffff877067170392f4e6125
Polkovnik-Daniil <0205danik@mail.ru>
2022-10-11 18:43:38 +0300

    version 0.1

--- main.py
+++ main.py
@@ -4,15 +4,66 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import seaborn as sns
 
-data = pd.read_csv("data/german_credit_data.csv")
-data.info()
-print(data[data.eq(0)].count())
+df = pd.read_csv("data/ds_salaries.csv")
+df.info()
+print(df[df.eq(0)].count())
 
-cols = data.columns[:]
-#определяем цвета
-#красный - пропущенные данные
-colours = ['#eeeeee','#00ff00']
+cols = df.columns[:]
+colors = ['#eeeeee', '#00ff00']
+sns.heatmap(df[cols].isnull(), cmap=sns.color_palette(colors))
 
-sns.heatmap(data[cols].isnull(), cmap=sns.color_palette(colours))
-plt.show()
-data.fillna(data.mean())
+df.isnull().any()
+
+for col in df.columns:
+    pct_missing = np.mean(df[col].isnull())
+    print('{} - {}%'.format(col, round(pct_missing*100)))
+
+df = df.drop(["Checking account"], axis=1)
+df = df.drop(["Purpose"], axis=1)
+df.head()
+
+for col in df.columns:
+    pct_missing = np.mean(df[col].isnull())
+    print('{} - {}%'.format(col, round(pct_missing*100)))
+
+
+df["Saving accounts"]=df["Saving accounts"].fillna("quite little")
+df["Saving accounts"]
+
+
+for col in df.columns:
+    pct_missing = df[col].isnull().sum()
+    print('{} - {}'.format(col, round(pct_missing)))
+
+sns.histplot(data=df["Saving accounts"], log_scale=(False, True), bins=30)
+
+
+start_df = pd.read_csv('D:\Program Files\Python\My CSV\german_credit_data.csv')
+sns.histplot(data=start_df["Saving accounts"], log_scale=(False, True), bins=30)
+
+
+
+# функция map() используется для применения функции к каждому элементу итерируемого объекта
+df["Saving accounts"]=df["Saving accounts"].map({"quite little": 0, "little": 1, "moderate": 2,"rich": 3, "quite rich": 4})
+df.head()
+
+
+df["Sex"]=df["Sex"].map({"male": 1, "female": 0})
+df["Housing"]=df["Housing"].map({"own": 0, "free": 1, "rent": 2})
+df.head()
+
+sns.boxplot(df['Age'])
+
+
+df['Age'].describe()
+
+
+print(np.where(df['Age']>65))
+df = df.apply(lambda x: x[(x>df.loc[0,x.name]) &
+                                    (x < df.loc[3,x.name])], axis=0)
+
+df.info()
+
+df = pd.DataFrame(df)
+
+df.to_csv('german_credit_data_clear.csv', index=False)
